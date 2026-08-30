367a7edb26ad625bb444a605d3f11e4d814ca015
sirgs <gustavo.soaress197@gmail.comm>
2026-08-29 22:21:07 -0300

    Adding Janus.IoC

---
+++ Janus.IoC/IoC.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Janus.IoC;
+
+public static class IoC
+{
+    public static IServiceCollection UpInfrastructure(this IServiceCollection service, IConfiguration configuration)
+    { service.AddControllers(); return service; }
+}
