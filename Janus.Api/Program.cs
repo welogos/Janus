7accd6f26957e0d0ca0c2979bdfe0c229917ca38
sirgs <gustavo.soaress197@gmail.comm>
2026-09-04 18:50:28 -0300

    Feat: Creating EndpointsController

--- Janus.Api/Program.cs
+++ Janus.Api/Program.cs
@@ -1,9 +1,12 @@
+using Janus.Api.Extensions;
 using Janus.IoC;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.UpInfrastructure(builder.Configuration);
 
 var app = builder.Build();
+
+await app.MigrateDatabaseAsync();
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
